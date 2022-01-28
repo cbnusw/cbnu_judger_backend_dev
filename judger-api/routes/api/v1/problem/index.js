e9704ccf356359db0cc6d72e1426260f15ef964e
vcho1958 <gusckd57@gmail.com>
2022-01-28 12:07:00 +0900

    Feat : 추가 problem/index.js hsaProblemPermission 미들웨어

--- judger-api/routes/api/v1/problem/index.js
+++ judger-api/routes/api/v1/problem/index.js
@@ -6,12 +6,9 @@ const { PARENT_TYPES } = require('../../../../constants');
 const [CONTEST, ASSIGNMENT] = PARENT_TYPES;
 const router = Router();
 
-router.get('/', controller.getProblems(null));
-router.get('/contest', controller.getProblems(CONTEST));
-router.get('/assignment', controller.getProblems(ASSIGNMENT));
+router.get('/', controller.getProblems);
 router.get('/:id', authenticate, handleAccessProblem, controller.getProblem);
-router.post('/', ...hasPermission('judge'), controller.createProblem(CONTEST));
-router.post('/assignment', ...hasRole('professor'), controller.createProblem(ASSIGNMENT))
+router.post('/', ...hasProblemPermission, controller.createProblem);
 // router.post('/:id/submit', controller.createSubmit);
 router.post('/:id/submit', isAuthenticated, handleAccessProblem, controller.createSubmit);
 router.put('/:id', ...hasPermission('judge'), controller.updateProblem);
